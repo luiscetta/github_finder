32e11967f70dde1e72fa1013414d340e52ae9d5c
luiscetta <filipe.cetta@gmail.com>
2023-11-25 10:18:37 -0300

    finishing create search modal

--- src/components/Modal/SearchModal/index.tsx
+++ src/components/Modal/SearchModal/index.tsx
@@ -1,5 +1,8 @@
 import { Modal } from "react-bootstrap";
+import Lottie from "lottie-react";
+import { BsSearch } from "react-icons/bs";
 
+import animationData from "../../assets/json/cat1.json";
 import "./styles.scss";
 
 type SearchModalProps = {
@@ -7,11 +10,11 @@ type SearchModalProps = {
   onHide: () => void;
 };
 
-export default function SearchModal({ show, onHide }: SearchModalProps) {
+const SearchModal = ({ show, onHide }: SearchModalProps) => {
   return (
     <>
       <Modal
-        size="lg"
+        size="xl"
         show={show}
         aria-labelledby="contained-modal-title-vcenter"
         centered
@@ -21,13 +24,35 @@ export default function SearchModal({ show, onHide }: SearchModalProps) {
           <Modal.Title>Finder</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4 className="input-title">Enter the user you want to search for below</h4>
-          <div className="search-input-field">
-            <input className="search-input" type="text" />
-            <button className="search-btn">BUSCAR</button>
+          <div className="input-area">
+            <h4 className="input-title">
+              Use the field below to do your search
+            </h4>
+            <div className="search-input-field">
+              <input
+                className="search-input"
+                type="text"
+                placeholder="Enter the user name."
+              />
+              <button className="search-btn">
+                Search
+                <BsSearch className="search-icon"/>
+              </button>
+            </div>
+          </div>
+          <div className="input-animation-area">
+            <Lottie
+              className="cat-animation"
+              animationData={animationData}
+              loop={true}
+              autoplay={true}
+            />
+            <h2 className="subtitle-animation">Found it!</h2>
           </div>
         </Modal.Body>
       </Modal>
     </>
   );
-}
+};
+
+export default SearchModal;
