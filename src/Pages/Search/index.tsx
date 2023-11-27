b7aead40a4a9bce8738f7664686f37482e4c8eab
luiscetta <filipe.cetta@gmail.com>
2023-11-27 12:56:26 -0300

    stylizing Search page

--- src/Pages/Search/index.tsx
+++ src/Pages/Search/index.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react";
 
 import SearchModal from "../../components/Modal/SearchModal";
+import animationData from "../../components/assets/json/backBtn.json";
 import "./styles.scss";
 
 export default function Search() {
@@ -12,10 +15,21 @@ export default function Search() {
 
   return (
     <div className="search-container">
+      <Link to="/" className="back-btn">
+        <Lottie
+          className="back-btn-animation"
+          animationData={animationData}
+          loop={true}
+          autoplay={true}
+        />
+      </Link>
       <div className="search-content">
-        <h2>buscar</h2>
+        <h2 className="search-title">
+          Do you already know which user you're going to search for? If so, just
+          click the button below!
+        </h2>
         <a className="search-btn" onClick={handleOpenModal}>
-          Search for user
+          Click here to search
         </a>
       </div>
       <SearchModal show={modalShow} onHide={() => setModalShow(false)} />
