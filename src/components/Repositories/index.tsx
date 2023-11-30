477c5f49b77bbdab8baa788415100c87c291340a
luiscetta <filipe.cetta@gmail.com>
2023-11-30 18:59:04 -0300

    adding stargazes and forks count on the  repo card

--- src/components/Repositories/index.tsx
+++ src/components/Repositories/index.tsx
@@ -1,6 +1,10 @@
+import { FaRegStar as Star } from "react-icons/fa";
+import { FaCodeFork as Fork } from "react-icons/fa6";
+
 import { GithubRepo } from "../../types/user";
-import "./styles.scss";
 import DotLng from "../DotLng";
+import FormatNumber from "../../utils/formatNumber";
+import "./styles.scss";
 
 type ReposComponentProps = {
   data: GithubRepo[];
@@ -19,6 +23,7 @@ const Repositories = ({ data }: ReposComponentProps) => {
                   <a className="repo-name" href={repo.html_url} target="_blank">
                     {repo.name}
                   </a>
+                  <span className="repo-visibility">{repo.visibility}</span>
                 </div>
                 <div className="repo-description">
                   {repo.description ? (
@@ -27,9 +32,23 @@ const Repositories = ({ data }: ReposComponentProps) => {
                     <p className="description">Sem descrição.</p>
                   )}
                 </div>
-                <footer className="repo-language">
-                  <DotLng language={repo.language} />
-                  <span className="lng">{repo.language}</span>
+                <footer className="repo-footer">
+                  <div className="lng-content">
+                    <DotLng language={repo.language} />
+                    <span className="lng">{repo.language}</span>
+                  </div>
+                  <div className="star-content">
+                    <Star className="star-icon" />
+                    <span className="star-count">
+                      {FormatNumber(repo.stargazers_count)}
+                    </span>
+                  </div>
+                  <div className="fork-content">
+                    <Fork className="fork-icon" />
+                    <span className="fork-count">
+                      {FormatNumber(repo.forks_count)}
+                    </span>
+                  </div>
                 </footer>
               </div>
             ))}
