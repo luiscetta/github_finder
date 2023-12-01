d5c05f101a3a1d6e99ae007dc3629f378060bfd7
luiscetta <filipe.cetta@gmail.com>
2023-12-01 09:32:27 -0300

    refactorin repo component & styles

--- src/components/Repositories/index.tsx
+++ src/components/Repositories/index.tsx
@@ -1,5 +1,6 @@
 import { FaRegStar as Star } from "react-icons/fa";
 import { FaCodeFork as Fork } from "react-icons/fa6";
+import { RiGitRepositoryLine } from "react-icons/ri";
 
 import { GithubRepo } from "../../types/user";
 import DotLng from "../DotLng";
@@ -21,20 +22,19 @@ const Repositories = ({ data }: ReposComponentProps) => {
             .map((repo) => (
               <div className="repo-card" key={repo.id}>
                 <div className="repo-card-title">
+                  <RiGitRepositoryLine className="repo-icon" />
                   <a className="repo-name" href={repo.html_url} target="_blank">
                     {repo.name}
                   </a>
                   <span className="repo-visibility">{repo.visibility}</span>
                 </div>
                 <div className="repo-description">
-                  {repo.description ? (
+                  {repo.description && (
                     <TruncateText
                       className="description"
                       text={repo.description}
                       maxLength={57}
                     />
-                  ) : (
-                    <p className="description">Sem descrição.</p>
                   )}
                 </div>
                 <footer className="repo-footer">
