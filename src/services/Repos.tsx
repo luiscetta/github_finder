0bd4059357d8233fab91836e0ce40dbcd72da367
luiscetta <filipe.cetta@gmail.com>
2023-12-16 12:42:45 -0300

    refactoring  GetRepos service promise

--- src/services/Repos.tsx
+++ src/services/Repos.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { GithubRepo } from "../types/user";
 
-async function GetRepos(userName: string): Promise<GithubRepo> {
+async function GetRepos(userName: string): Promise<GithubRepo[]> {
   const res = await axios.get(`https://api.github.com/users/${userName}/repos`);
   return res.data;
 }
